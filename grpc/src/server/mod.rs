56323f114673edf7c3206fbca70b00579894231c
Saurav <sauravz@google.com>
2026-04-09 10:13:22 +0000

    Add interceptor API

--- grpc/src/server/mod.rs
+++ grpc/src/server/mod.rs
@@ -32,6 +32,8 @@ use crate::core::ServerResponseStreamItem;
 use crate::core::Trailers;
 use tokio::sync::oneshot;
 
+pub mod interceptor;
+
 pub struct Server {
     handler: Option<Arc<dyn DynHandle>>,
 }
